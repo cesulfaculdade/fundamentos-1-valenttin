36a7fd995b5737223465937560bfa9aa071a1cd0
Valenttin-cesul <valenttin.2023014082@alunocesul.com>
2024-08-05 22:59:12 -0300

    criação do input e dos textos

---
+++ App.tsx
@@ -0,0 +1,9 @@
+
+import { Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+      <Home />
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,72 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Text, TextBase, TextInput } from "react-native";
+
+export function Home() {
+  const handleProductAdd = () => {
+    console.log("Adicionei o produto");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Compras</Text>
+      <Text style={styles.todaysDate}>Segunda-feira, 05 de Agosto de 2024</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Produto"
+          placeholderTextColor="#BDBABA"
+          keyboardType="default"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleProductAdd}>
+          <Text style={styles.textButton}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EAEAEA",
+    padding: 24,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#fff",
+    height: 56,
+    padding: 16,
+    fontSize: 16,
+    borderRadius: 5,
+    marginRight: 16,
+  },
+  button: {
+    backgroundColor: "#31C667",
+    width: 56,
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+  },
+  textButton: {
+    fontSize: 24,
+    color: "#fff",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 18,
+  },
+  title: {
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  todaysDate: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "normal",
+  },
+});
